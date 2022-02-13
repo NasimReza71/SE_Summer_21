209b904bd897cb68af0f49de517aac4179bcaeee
NasimReza71 <nasim.reza201@gmail.com>
2022-02-13 21:28:06 +0600

    JustTry

--- Test1.php
+++ Test1.php
@@ -2,6 +2,10 @@
 <head>
 </head>
 <body>
-    <h1>hello</h1>
+    <h1>Hello</h1>
+    <h2>This is Nasim Reza</h2>
+    <?php echo "So what?"?><br>
+    <h3><?php echo "PHP uses for temporary storage"?><h3>
+
 </body>
 </html>
